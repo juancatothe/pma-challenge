9b1cd73de91ce2246902b33f70e68045cdb1b454
Juan Carlos <juanthewebdeveloper@gmail.com>
2019-10-07 00:14:04 -0400

    Updates

---
+++ precache-manifest.032b2b408cac9d6d99a661da9087d73f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725468ef3756f5e3fd68af83d6b4b7c2",
+    "url": "/react-task-app/index.html"
+  },
+  {
+    "revision": "9196e2ed362eb4c0e261",
+    "url": "/react-task-app/static/css/main.238e1323.chunk.css"
+  },
+  {
+    "revision": "31cc4219f1442f7853b0",
+    "url": "/react-task-app/static/js/2.a350d121.chunk.js"
+  },
+  {
+    "revision": "9196e2ed362eb4c0e261",
+    "url": "/react-task-app/static/js/main.1475e5e2.chunk.js"
+  },
+  {
+    "revision": "5ebe5e1d9589ec2878ae",
+    "url": "/react-task-app/static/js/runtime-main.a0fa2bef.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-task-app/static/media/logo.25bf045c.svg"
+  }
+]);
